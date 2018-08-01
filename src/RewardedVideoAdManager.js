68dcaf98bb019fcc82c9b1e73b3037906656193d
princecharming519 <apollo519@outlook.com>
2018-08-01 11:03:20 -0400

    Fix rewarded

--- src/RewardedVideoAdManager.js
+++ src/RewardedVideoAdManager.js
@@ -13,15 +13,10 @@ import { NativeModules } from 'react-native';
 const { CTKRewardedVideoAdManager } = NativeModules;
 
 export default {
-  /**
-   * loads rewarded video ad for a given placementId
-   */
   loadAd(placementId: string): Promise<boolean> {
     return CTKRewardedVideoAdManager.loadAd(placementId);
   },
-  /**
-   * shows the loaded rewarded video ad, returns a promise
-   */
+
   showAd(): Promise<boolean> {
     return CTKRewardedVideoAdManager.showAd();
   },
